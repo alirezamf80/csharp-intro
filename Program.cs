644fbc2583af3912e9c582539363a1365e69ad3b
alireza manafvand <azzzzmf80@gmail.com>
2020-11-23 17:40:41 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello alireza");
+        }
+    }
+}
